a7674bcf503355333ab0fb1e3e341a46da620339
unknown <sanung2002@gmail.com>
2022-08-27 12:11:53 +0700

    End of Section 2 Api Basic

---
+++ API/Program.cs
@@ -0,0 +1,49 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<StoreContext>(opt=>{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+#region //สร้ํางข้อมูลจ ําลอง Fake data
+    using var scope = app.Services.CreateScope(); //using หลังท ํางํานเสร็จจะถูกท ําลํายจํากMemory
+    var context = scope.ServiceProvider.GetRequiredService<StoreContext>();
+    var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+    try
+    {
+    await context.Database.MigrateAsync(); //สร้ําง DB ให้อัตโนมัติถ้ํายังไม่มี
+    await DbInitializer.Initialize(context); //สร้ํางข้อมูลสินค้ําจ ําลอง
+    }
+    catch (Exception ex)
+    {
+    logger.LogError(ex, "Problem migrating data");
+    }
+#endregion
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+//app.UseHttpsRedirection(); web
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+await app.RunAsync();
